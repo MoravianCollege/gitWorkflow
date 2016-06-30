441d7d40612155d2eb84ccfafd0d63fe834cb153
zambeezy <stnvz01@moravian.edu>
2016-06-30 11:43:34 -0400

    Reformat the PlayRound() method. Add two private methods, RemoveSetFromTable() and AddCardsToTable().

--- Game.java
+++ Game.java
@@ -1,108 +1,89 @@
 //This class plays one game of set
-public class Game
-{
+public class Game {
   private Table t;
   private Deck d;
 
   //This constructor plays a game of set using a standard deck
-  public Game()
-  {
+  public Game() {
     t = new Table();
     d = new Deck();
-    
+
     // There are always 81 cards in a new Deck, so this can't fail
-    for(int i = 0; i < 12; i++)
+    for (int i = 0; i < 12; i++)
       t.add(d.getNext());
   }
 
   //This constructor plays a game of set using a specific deck
-  public Game(String filename)
-  {
+  public Game(String filename) {
     t = new Table();
     d = new Deck(filename);
-    
+
     // There might not be 12 cards
-    while(d.hasNext() && t.numCards() < 12)
+    while (d.hasNext() && t.numCards() < 12)
       t.add(d.getNext());
   }
-  
-  public int numSets()
-  {
+
+  public int numSets() {
     return t.numSets();
   }
-  
-  public int numCards()
-  {
+
+  public int numCards() {
     return t.numCards();
   }
-  
-  public boolean isGameOver()
-  {
-    if(d.hasNext())
+
+  public boolean isGameOver() {
+    if (d.hasNext())
       return false;
-    
+
     // if we get here, there are no more cards in the deck
     // the game continues until there are no sets on the table
     return t.numSets() == 0;
   }
-  
-  public void playRound()
-  {
-    // If the game is over, they shouldn't have called this...
-    if(isGameOver())
-      return;
 
-    // If there are no sets, we have to add cards
-    if(t.numSets() == 0)
-    {
-      // There must be at least one card
-      t.add(d.getNext());
-      
-      // If we have a custom deck, it might not have a multiple
-      // of three cards.
-      if(d.hasNext())
-        t.add(d.getNext());
-      if(d.hasNext())
-        t.add(d.getNext());
-      return;
-    }
-    
-    // There is a set on the table...
-    for(int i = 0; i < t.numCards(); i++)
-    {
-      for(int j = i + 1; j < t.numCards(); j++)
-      {
-        for(int k = j + 1; k < t.numCards(); k++)
-        {
-          Card c1 = t.getCard(i);
-          Card c2 = t.getCard(j);
-          Card c3 = t.getCard(k);
-          
-          if(Card.isSet(c1, c2, c3))
-          {
-            t.removeSet(c1, c2, c3);
-
-            // If there are 12 or more cards on the table,
-            // we do not add cards this round.
-            if(t.numCards() < 12)
-            {
-              // there may or may not be cards left in the deck
-              if(d.hasNext())
-                t.add(d.getNext());
-            
-              if(d.hasNext())
-                t.add(d.getNext());
-              
-              if(d.hasNext())
-                t.add(d.getNext());
-            }
-            
-            // regardless of whether we add cards, we are done
-            // when we find a set.
-            return;
+  /** Removes a set from the table
+   * iterates over the table until it finds a set
+   * once set is found it is removed and the loop is broken
+   */
+  private void removeSetFromTable() {
+    loop:
+    for (int x = 0; x < t.numCards() - 2; x++) {
+      for (int y = x + 1; y < t.numCards() - 1; y++) {
+        for (int z = y + 1; z < t.numCards(); z++) {
+          if (Card.isSet(t.getCard(x), t.getCard(y), t.getCard(z))) {
+            t.removeSet(t.getCard(x), t.getCard(y), t.getCard(z));
+            break loop;
           }
         }
       }
+    }
+  }
+
+  /** Add cards to the table
+   * one at a time until there are no cards left in the deck
+   * or it has added 3 cards
+   */
+  private void addCardsToTable() {
+    int i = 0;
+    while (d.hasNext() && i < 3) {
+      t.add(d.getNext());
+      i += 1;
+    }
+  }
+
+  public void playRound() {
+    if (t.numSets() == 0 && d.hasNext()) {
+      this.addCardsToTable();
+    }
+    else if (t.numSets() > 0 && t.numCards() == 12) {
+      this.removeSetFromTable();
+      if (d.hasNext())
+        this.addCardsToTable();
+    }
+    else if (t.numSets() > 0 && t.numCards() > 12) {
+      this.removeSetFromTable();
+    }
+    else if (t.numSets() > 0 && !d.hasNext()) {
+      this.removeSetFromTable();
     }
   }
 }
